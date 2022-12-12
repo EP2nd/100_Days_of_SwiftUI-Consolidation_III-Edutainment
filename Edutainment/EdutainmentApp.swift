ccc4b96b24f0d72c808fe897b8d2cbae73bedd2b
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-12-12 20:18:29 +0100

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Edwin Przeźwiecki Jr. on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

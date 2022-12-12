fb9f223ca0e28af9cb5a7e4e06454c926513ad32
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-12-12 22:24:22 +0100

    Groundwork

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var numberToMultiply = 2
+    @State private var multiplier = Int.random(in: 2...10)
+    @State private var numberOfQuestions = 3
+    
+    @State private var score = 0
+    
+    @State private var startGame = false
+    
+    @State private var answer = ""
+    
+    var outcome: Int {
+        
+        let outcome = numberToMultiply * multiplier
+        
+        return outcome
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Stepper("\(numberToMultiply)", value: $numberToMultiply, in: 2...10)
+                } header: {
+                    Text("Choose the number to multiply:")
+                }
+                
+                Section {
+                    Stepper("\(numberOfQuestions)", value: $numberOfQuestions, in: 2...20)
+                } header: {
+                    Text("Choose the amount of questions:")
+                }
+                
+                Button("Start") {
+                    startGame.toggle()
+                }
+            }
+            .navigationTitle("Edutainment")
+            .toolbar {
+                Text("Score: \(score)")
+            }
         }
-        .padding()
+        .alert("Question", isPresented: $startGame) {
+            TextField("Enter the value", text: $answer)
+            Button("Answer", action: answerCheck)
+        } message: {
+            Text("\(numberToMultiply) x \(multiplier)?")
+        }
+    }
+    
+    func answerCheck() {
+        if answer == String(outcome) {
+            score += 1
+        } else {
+            score -= 1
+        }
     }
 }
 
